d7677648fe36be70c79c7a693c4e0d8fdf9d858f
Max De Jesus <me@maxdejesus.com>
2019-07-24 22:15:21 -0400

    deleted mongodb dependency

---
+++ app.js
@@ -0,0 +1,56 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'brStack'
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+// Use connect method to connect to the Server
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+    assert.equal(null, err);
+    console.log('Succesfully connected to the server');
+
+    const db = client.db(dbName);
+
+    insertDocuments(db, function() {
+        findDocuments(db, function(){
+            client.close();
+        });
+    });
+
+    client.close();
+});
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('keywords');
+    // Insert some documents
+    collection.insertMany([
+      {a : 1}, {a : 2}, {a : 3}
+    ], function(err, result) {
+      assert.equal(err, null);
+      assert.equal(3, result.result.n);
+      assert.equal(3, result.ops.length);
+      console.log('Inserted 3 documents into the collection');
+      callback(result);
+    });
+  }
+
+  const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('keywords');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(docs)
+      callback(docs);
+    });
+  }
